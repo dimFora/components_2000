132c7c62f8294fb751e44c44bd9865bb68433da4
Artem Mezin <iketari@gmail.com>
2016-10-03 21:19:58 +0300

    menu render

--- components/app/app.js
+++ components/app/app.js
@@ -5,8 +5,28 @@
 	let Menu = window.Menu;
 
 
+	let menuData = {
+		title: 'Рабочие ссылки',
+		items: [
+			{
+				anchor: 'mail.ru'
+			},
+			{
+				anchor: 'yandex.ru'
+			},
+			{
+				anchor: 'yahoo.com'
+			},
+			{
+				anchor: 'google.com'
+			}
+		]
+	};
+
+
 	let menu = new Menu({
-		el: document.querySelector('.js-menu')
+		el: document.querySelector('.js-menu'),
+		data: menuData
 	});
 
 })();
